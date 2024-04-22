d49422916d8c2081a0a6c3aba700a64636c7edff
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-04-22 12:13:33 +0300

    change the admin transaction payload

--- tenant_services/serializers.py
+++ tenant_services/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from authentication.api.serializers import UserProfileSerializer
-from authentication.models import Tenant
-from properties.models import PropertyBlock
+from authentication.models import Tenant, User
+from properties.models import Property, PropertyBlock
 
 from tenant_services.models import services
 
@@ -16,15 +16,41 @@ class PayServiceSerializer(serializers.Serializer):
     mobile_number = serializers.CharField(required=False, allow_blank=True)
     pay_via = serializers.CharField()
 
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ('block_number',)
+
+class PropertyBlockSerializer(serializers.ModelSerializer):
+    block = PropertySerializer()
+    class Meta:
+        model = PropertyBlock
+        fields = ('house_number', 'block')
+
 
 class TenantSerializer(serializers.ModelSerializer):
-    id_number = serializers.CharField(read_only=True)
+    PropertyBlock = PropertyBlockSerializer()
     class Meta:
         model = Tenant
-        fields = ['name', 'id_number', 'is_active', 'PropertyBlock']
+        fields = ('PropertyBlock',)
+
+class UserSerializer(serializers.ModelSerializer):
+    tenant = TenantSerializer(source='tenant.all', many=True)
+
+    def get_tenant(self, obj):
+        # Access the related Tenant instance through the user's tenant relation
+        tenant_instance = obj.tenant.first()  # Assuming there's only one tenant per user
+        if tenant_instance:
+            return TenantSerializer(tenant_instance).data
+        return None
+
+    class Meta:
+        model = User
+        fields = ('tenant', 'email', 'username', 'role', 'mobile_number', 'status')
+
 
 class AllTRansactionsSerializer(serializers.ModelSerializer):
-    user = UserProfileSerializer()
+    user = UserSerializer()
     class Meta:
         model = services
         fields = '__all__'
